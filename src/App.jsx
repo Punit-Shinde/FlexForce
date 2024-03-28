599ac5a25b73da61ce42e8ac3d28e91e3d0712b0
Punit Shinde <punitshinde1@gmail.com>
2024-03-28 13:25:34 +0530

    Initial setup of pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+//Css
+import "./App.css";
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+//Pages
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
-export default App
+export default App;
