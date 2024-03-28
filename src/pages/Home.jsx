adbe3baf9174ff265f751e8b3ef7885285fdb1bd
Punit Shinde <punitshinde1@gmail.com>
2024-03-28 14:04:17 +0530

    Hero Banner and Search Excersice UI is done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+//Components
+import HeroBanner from '../components/HeroBanner.jsx';
+import SearchExercises from '../components/SearchExercises.jsx';
+import Exercises from '../components/Exercises.jsx';
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <Box>
+    <HeroBanner/>
+    <SearchExercises/>
+    <Exercises/>
+   </Box>
   )
 }
 
